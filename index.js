41caff0143cb36c2bc428be77925bb4e13bf02ec
Jorge Navarro <jnavarr56@gmail.com>
2019-04-11 16:55:54 -0400

    Added commenting, implementing passport google strategy.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
+/*
 app.get('/', (req, res) => {
 
     res.send({ testing: 'hey mang' });
 
 });
+*/
 
+/*
+REMEMBER UDEMY CORRECTION
+http://localhost:5000/auth/google/callback
+*/
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);   
